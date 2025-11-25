3543a84f2003b1e1ab78964d7ff8f69679438c08
Monisha_Ishwar <142509375+123monisha@users.noreply.github.com>
2025-11-25 13:06:46 +0530

    Update DS3.cs

--- DS3.cs
+++ DS3.cs
@@ -17,7 +17,35 @@
 //                {
 //                    Console.WriteLine(item);
 //                }
-//           Console.WriteLine( stack.Peek());
+//           Console.WriteLine( stack.Peek());using System;
+using System.Collections.Generic;
+
+namespace Daily_Practice
+{
+    internal class DS3
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Krishna");
+            stack.Push("Radha");
+            stack.Push("Monisha");
+
+            stack.Pop();
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(stack.Peek());
+            int count = stack.Count;
+            Console.WriteLine(count);
+
+
+        }
+    }
+}
+
+
 //           int count= stack.Count;
 //            Console.WriteLine(count);
 
@@ -25,3 +53,4 @@
 //        }
 //    }
 //}
+
